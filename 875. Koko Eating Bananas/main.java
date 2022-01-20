3551d225f37c660c90239725b7ab4a2881064f3a
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2022-01-20 21:25:07 +0900

    Create main.java

---
+++ 875. Koko Eating Bananas/main.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1;
+        for(int pile: piles){
+            right = Math.max(right, pile);
+        }
+        
+        while(left < right){
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+            
+            for(int pile : piles){
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+            
+            if(hourSpent <= h){
+                right = middle;
+            }else{
+                left = middle + 1;
+            }
+        }
+        // that is, the minimum workable eating speed
+        return right;
+    }
+}
