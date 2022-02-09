fb6e3b1664e0950882cbcdeb60d3acac1f5c19b5
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2022-02-09 23:17:47 +0900

    Create main.java

---
+++ 532. K-diff Pairs in an Array/main.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int count = 0;
+        for(int x : map.keySet()){
+            if(k>0 && map.containsKey(x + k) || k == 0 && map.get(x) > 1) count++;
+        }
+        return count;
+    }
+}
