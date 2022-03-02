f9817cbacdb8eea91b61091699562ae751f5b999
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2022-03-02 12:36:40 +0900

    Create main.java

---
+++ 392. Is Subsequence/main.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIdx = 0, tIdx = 0;
+        
+        while(tIdx < t.length()){
+            if(sIdx == s.length()) break;
+            if(s.charAt(sIdx) == t.charAt(tIdx)){
+                sIdx++;
+                tIdx++;
+            }
+            else tIdx++;
+        }
+        return sIdx == s.length();
+    }
+}
