4695647a932dd59c7edfbbcbdf7fd864df7500fb
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-10-07 00:41:12 +0900

    Create main.java

---
+++ 343. Integer Break/main.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        
+        if (n % 3 == 0) {
+            return (int) Math.pow(3, n / 3);
+        }
+        
+        if (n % 3 == 1) {
+            return (int) Math.pow(3, (n / 3 - 1)) * 4;
+        }
+        
+        return (int) Math.pow(3, n / 3) * 2;
+    }
+}
