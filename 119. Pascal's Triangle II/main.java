65b8755395952bfe34fd83bfeb791110b43c5a99
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-10-16 09:08:10 +0900

    Update main.java

--- 119. Pascal's Triangle II/main.java
+++ 119. Pascal's Triangle II/main.java
@@ -16,3 +16,21 @@ class Solution {
         return row;
     }
 }
+
+
+// Second solution
+
+class Solution {
+   public  List<Integer> getRow(int rowIndex) {
+        Integer[] a=new Integer[rowIndex+1];
+        Arrays.fill(a,0);
+        a[0]=1;
+        for (int i=1;i<=rowIndex;i++)
+        {
+            for (int j=i;j>0;j--){
+                a[j]=a[j]+a[j-1];
+            }
+        }
+        return Arrays.asList(a);
+    }
+}
