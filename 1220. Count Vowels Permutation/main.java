9204d5b6c2d35220791a5c2f8ba515e1b58711e1
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2023-10-28 17:35:18 +0900

    Create main.java

---
+++ 1220. Count Vowels Permutation/main.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int countVowelPermutation(int n) {
+        final int MOD = 1000000007;
+        
+        long countA = 1, countE = 1, countI = 1, countO = 1, countU = 1;
+        
+        for (int length = 1; length < n; length++) {
+            long nextCountA = countE;
+            long nextCountE = (countA + countI) % MOD;
+            long nextCountI = (countA + countE + countO + countU) % MOD;
+            long nextCountO = (countI + countU) % MOD;
+            long nextCountU = countA;
+            
+            countA = nextCountA;
+            countE = nextCountE;
+            countI = nextCountI;
+            countO = nextCountO;
+            countU = nextCountU;
+        }
+        long totalCount = (countA + countE + countI + countO + countU) % MOD;
+        
+        return (int) totalCount;
+    }
+}
