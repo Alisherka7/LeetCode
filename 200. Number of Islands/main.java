2f279b30c9e05e4467297ace080c076f9e828839
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2024-04-20 17:08:38 +0900

    Create main.java

---
+++ 200. Number of Islands/main.java
@@ -0,0 +1,31 @@
+class Solution {
+    int numIslands(char[][] grid) {
+    int res = 0;
+    int m = grid.length, n = grid[0].length;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == '1') {
+                res++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+    return res;
+    }
+
+    void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+}
