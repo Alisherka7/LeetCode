b11d33bb82c9f7cce346d113e041af34fda90e27
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2024-06-15 23:52:26 +0900

    Create main.java

---
+++ 502. IPO/main.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        List<int[]> projects = new ArrayList<>();
+        for (int i = 0; i < profits.length; i++) {
+            projects.add(new int[]{capital[i], profits[i]});
+        }
+        
+        projects.sort((a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < projects.size(); i++) {
+            if (w >= projects.get(i)[0]) {
+                maxHeap.offer(projects.get(i)[1]);
+            } else if (k > 0 && !maxHeap.isEmpty()) {
+                k--;
+                i--;
+                w += maxHeap.poll();
+            }
+        }
+        
+        while (!maxHeap.isEmpty() && k > 0) {
+            w += maxHeap.poll();
+            k--;
+        }
+        
+        return w;
+    }
+}
