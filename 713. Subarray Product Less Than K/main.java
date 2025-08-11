5047970dc840c6f265b31d88727b97c4a8d3c92e
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2025-08-11 23:55:58 +0900

    Update main.java

--- 713. Subarray Product Less Than K/main.java
+++ 713. Subarray Product Less Than K/main.java
@@ -1,29 +1,20 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k == 0) return 0;
-        double logK = Math.log(k);
-        int m = nums.length + 1;
-        double[] logsPrefixSum = new double[m];
-        
-        // Calculate prefix sum of logarithms of elements
-        for (int i = 0; i < nums.length; i++) {
-            logsPrefixSum[i + 1] = logsPrefixSum[i] + Math.log(nums[i]);
+        if (k <= 1) return 0;
+        int windowStart = 0;
+        int prod = 1;
+        int ans = 0;
+        for(int windowEnd = 0; windowEnd < nums.length; windowEnd++){
+            prod *= nums[windowEnd];
+
+            while(prod >= k){
+                prod /= nums[windowStart];
+                windowStart++;
+            }
+
+            ans += (windowEnd - windowStart) + 1;
         }
 
-        int totalCount = 0;
-        // Calculate subarray count with product less than k
-        for (int currIdx = 0; currIdx < m; currIdx++) {
-            int low = currIdx + 1, high = m;
-            while (low < high) {
-                int mid = low + (high - low) / 2;
-                if (logsPrefixSum[mid] < logsPrefixSum[currIdx] + logK - 1e-9) {
-                    low = mid + 1;
-                } else {
-                    high = mid;
-                }
-            }
-            totalCount += low - currIdx - 1;
-        }
-        return totalCount;
+        return ans;
     }
 }
