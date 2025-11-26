1b6a28f33d236b3027cb45535a823e2df02e3ae2
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2025-11-26 18:35:51 +0900

    Implement numberOfPaths method for matrix paths

---
+++ 2435. Paths in Matrix Whose Sum Is Divisible by K/main.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    private static final int MOD = 1000000007;
+
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        long[][][] dp = new long[m + 1][n + 1][k];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    dp[i][j][grid[0][0] % k] = 1;
+                    continue;
+                }
+
+                int value = grid[i - 1][j - 1] % k;
+                for (int r = 0; r < k; r++) {
+                    int prevMod = (r - value + k) % k;
+                    dp[i][j][r] =
+                        (dp[i - 1][j][prevMod] + dp[i][j - 1][prevMod]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[m][n][0];
+    }
+}
