753b76f69325e57577e388112a972ab8142ff2a4
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2026-03-28 23:56:04 +0900

    Implement solution for finding string with LCP

---
+++ 2573. Find the String with LCP/main.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findTheString(self, lcp: List[List[int]]) -> str:
+        n = len(lcp)
+        word = [""] * n
+        current = ord("a")
+
+        for i in range(n):
+            if not word[i]:
+                if current > ord("z"):
+                    return ""
+                word[i] = chr(current)
+                for j in range(i + 1, n):
+                    if lcp[i][j]:
+                        word[j] = word[i]
+                current += 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if word[i] != word[j]:
+                    if lcp[i][j]:
+                        return ""
+                else:
+                    if i == n - 1 or j == n - 1:
+                        if lcp[i][j] != 1:
+                            return ""
+                    else:
+                        if lcp[i][j] != lcp[i + 1][j + 1] + 1:
+                            return ""
+
+        return "".join(word)
