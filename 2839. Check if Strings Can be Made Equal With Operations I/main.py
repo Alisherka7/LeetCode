023a0de987b28b61398c83913f009c112faa76e2
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2026-03-29 23:27:57 +0900

    Implement solution to check if strings can be equal

---
+++ 2839. Check if Strings Can be Made Equal With Operations I/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        even = (s1[0] == s2[0] and s1[2] == s2[2]) or \
+               (s1[0] == s2[2] and s1[2] == s2[0])
+
+        odd = (s1[1] == s2[1] and s1[3] == s2[3]) or \
+              (s1[1] == s2[3] and s1[3] == s2[1])
+
+        return even and odd
